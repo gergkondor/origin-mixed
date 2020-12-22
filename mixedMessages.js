76478f5f4c53f71e2941af9334e4967bf75ca9c6
gergkondor <kondor.gerg@gmail.com>
2020-12-22 11:40:41 +0000

    add facts, numbers

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,6 +1,12 @@
-const quotes = ["All that we are is the result of what we have thought.",
+const obj = { quotes: ["All that we are is the result of what we have thought.",
                 "A jug fills drop by drop.",
-                "Showing off is the fool’s idea of glory."]
+                "Showing off is the fool’s idea of glory."],
+
+              facts: ["Earth is the only known planet with plate tectonics.",
+                      "Only one two-billionth of the Sun’s energy hits the Earth.",
+                      "Venus spins backward on its axis compared to all the other planets in our solar system."],
+              numbers: [42, 51, 3]
+};
 
 console.log(quotes[0]);
 console.log(quotes[1]);
