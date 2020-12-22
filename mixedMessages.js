04c91861f48a93356a7b9c5f6ef63bbbf27b5382
gergkondor <kondor.gerg@gmail.com>
2020-12-22 11:08:24 +0000

    Add quotes

---
+++ mixedMessages.js
@@ -0,0 +1,4 @@
+const quotes = ["All that we are is the result of what we have thought.",
+                "A jug fills drop by drop.",
+                "Showing off is the fool’s idea of glory."]
+
