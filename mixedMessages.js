e705eb74efb4d690148b86887d036e354a944891
gergkondor <kondor.gerg@gmail.com>
2020-12-22 11:27:15 +0000

    Log quote 2

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,3 +3,4 @@ const quotes = ["All that we are is the result of what we have thought.",
                 "Showing off is the fool’s idea of glory."]
 
 console.log(quotes[0]);
+console.log(quotes[1]);
