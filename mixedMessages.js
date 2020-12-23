a6ba582dbb3d5df86c1aa0ae012d3e2c53ca9dc8
gergkondor <kondor.gerg@gmail.com>
2020-12-23 11:43:09 +0000

    Add whitespace

--- mixedMessages.js
+++ mixedMessages.js
@@ -5,18 +5,20 @@ const obj = { quotes: ["All that we are is the result of what we have thought.",
               facts: ["Earth is the only known planet with plate tectonics.",
                       "Only one two-billionth of the Sun’s energy hits the Earth.",
                       "Venus spins backward on its axis compared to all the other planets in our solar system."],
+
               numbers: [42, 51, 3],
 
               randomQuotes() {
                 return this.quotes[Math.floor(Math.random() * this.quotes.length)]
               },
+
               randomFacts() {
                 return this.facts[Math.floor(Math.random() * this.facts.length)]
               },
+
               randomNumbers() {
                 return this.numbers[Math.floor(Math.random() * this.numbers.length)]
               }
-
-
 };
+
 console.log(`${obj.randomQuotes()} ${obj.randomFacts()} Your lucky number for today: ${obj.randomNumbers()}`)
