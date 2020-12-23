353f8256f4b33d709cd292d085676c3f1f5d8bf7
gergkondor <kondor.gerg@gmail.com>
2020-12-23 11:38:37 +0000

    Add random functions

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,12 +1,21 @@
 const obj = { quotes: ["All that we are is the result of what we have thought.",
-                "A jug fills drop by drop.",
-                "Showing off is the fool’s idea of glory."],
+                       "A jug fills drop by drop.",
+                       "Showing off is the fool’s idea of glory."],
 
               facts: ["Earth is the only known planet with plate tectonics.",
                       "Only one two-billionth of the Sun’s energy hits the Earth.",
                       "Venus spins backward on its axis compared to all the other planets in our solar system."],
-              numbers: [42, 51, 3]
-};
+              numbers: [42, 51, 3],
 
-console.log(quotes[0]);
-console.log(quotes[1]);
+              randomQuotes() {
+                return this.quotes[Math.floor(Math.random() * this.quotes.length)]
+              },
+              randomFacts() {
+                return this.facts[Math.floor(Math.random() * this.facts.length)]
+              },
+              randomNumbers() {
+                return this.numbers[Math.floor(Math.random() * this.numbers.length)]
+              }
+
+
+};
